f132be7f02015289aa11bd34cfea1dd23ae6403f
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-03-09 20:56:55 +0100

    Closes #26

--- src/main/java/mythicbotany/jei/InfusionCategory.java
+++ src/main/java/mythicbotany/jei/InfusionCategory.java
@@ -11,23 +11,19 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import mythicbotany.ModBlocks;
 import mythicbotany.MythicBotany;
 import mythicbotany.infuser.IInfuserRecipe;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector2f;
 import vazkii.botania.client.core.handler.HUDHandler;
-import vazkii.botania.client.integration.jei.petalapothecary.PetalApothecaryRecipeCategory;
+import vazkii.botania.client.integration.jei.PetalApothecaryRecipeCategory;
+import vazkii.botania.client.integration.jei.TerraPlateDrawable;
 
 import javax.annotation.Nonnull;
-import java.awt.*;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class InfusionCategory implements IRecipeCategory<IInfuserRecipe> {
@@ -38,12 +34,15 @@ public class InfusionCategory implements IRecipeCategory<IInfuserRecipe> {
     private final String localizedName;
     private final IDrawable overlay;
     private final IDrawable icon;
+    private final IDrawable infuserPlate;
 
     public InfusionCategory(IGuiHelper guiHelper) {
-        this.background = guiHelper.createBlankDrawable(150, 120);
+        this.background = guiHelper.createBlankDrawable(114, 131);
         this.localizedName = I18n.format("block.mythicbotany.mana_infuser");
-        this.overlay = guiHelper.createDrawable(new ResourceLocation("botania", "textures/gui/petal_overlay.png"), 0, 0, 150, 110);
+        this.overlay = guiHelper.createDrawable(new ResourceLocation("botania","textures/gui/terrasteel_jei_overlay.png"), 42, 29, 64, 64);
         this.icon = guiHelper.createDrawableIngredient(new ItemStack(ModBlocks.manaInfuser));
+        IDrawable shimmerrock = guiHelper.createDrawableIngredient(new ItemStack(vazkii.botania.common.block.ModBlocks.shimmerrock));
+        this.infuserPlate = new TerraPlateDrawable(shimmerrock, shimmerrock, guiHelper.createDrawableIngredient(new ItemStack(Blocks.GOLD_BLOCK)));
     }
 
     @Nonnull
@@ -77,7 +76,7 @@ public class InfusionCategory implements IRecipeCategory<IInfuserRecipe> {
     }
 
     @Override
-    public void setIngredients(IInfuserRecipe recipe, @Nonnull IIngredients ii) {
+    public void setIngredients(@Nonnull IInfuserRecipe recipe, @Nonnull IIngredients ii) {
         List<List<ItemStack>> list = new ArrayList<>();
         for (Ingredient ingredient : recipe.getIngredients()) {
             list.add(Arrays.asList(ingredient.getMatchingStacks()));
@@ -86,36 +85,30 @@ public class InfusionCategory implements IRecipeCategory<IInfuserRecipe> {
         ii.setOutput(VanillaTypes.ITEM, recipe.getRecipeOutput());
     }
 
+    @Override
+    public void setRecipe(@Nonnull IRecipeLayout layout, @Nonnull IInfuserRecipe recipe, @Nonnull IIngredients ii) {
+        layout.getItemStacks().init(0, false, 48, 37);
+        layout.getItemStacks().set(0, ii.getOutputs(VanillaTypes.ITEM).get(0));
+        double angle = 360 / (double) ii.getInputs(VanillaTypes.ITEM).size();
+        Vector2f point = new Vector2f(48, 5);
+        Vector2f center = new Vector2f(48, 37);
+        for (int i = 1; i <= ii.getInputs(VanillaTypes.ITEM).size(); i++) {
+            layout.getItemStacks().init(i, true, Math.round(point.x), Math.round(point.y));
+            layout.getItemStacks().set(i, ii.getInputs(VanillaTypes.ITEM).get(i - 1));
+            point = PetalApothecaryRecipeCategory.rotatePointAbout(point, center, angle);
+        }
+        layout.getItemStacks().init(ii.getInputs(VanillaTypes.ITEM).size() + 1, true, 48, 92);
+        layout.getItemStacks().set(ii.getInputs(VanillaTypes.ITEM).size() + 1, new ItemStack(ModBlocks.manaInfuser));
+    }
+
     @SuppressWarnings("deprecation")
     public void draw(IInfuserRecipe recipe, @Nonnull MatrixStack matrixStack, double mouseX, double mouseY) {
         RenderSystem.enableAlphaTest();
         RenderSystem.enableBlend();
-        this.overlay.draw(matrixStack);
-        HUDHandler.renderManaBar(matrixStack, 24, 105, 255, 0.75F, recipe.getManaUsage(), 8000000);
+        this.overlay.draw(matrixStack, 25, 14);
+        HUDHandler.renderManaBar(matrixStack, 6, 126, 255, 0.75f, recipe.getManaUsage(), 4000000);
+        this.infuserPlate.draw(matrixStack, 35, 92);
         RenderSystem.disableBlend();
         RenderSystem.disableAlphaTest();
-        FontRenderer fontRenderer = Minecraft.getInstance().fontRenderer;
-        String text = BigDecimal.valueOf(recipe.getManaUsage() / (double) 1000000).setScale(2, RoundingMode.HALF_UP).toPlainString() + " Mana Pools";
-        fontRenderer.drawString(matrixStack, text, (float) (75 - fontRenderer.getStringWidth(text) / 2), 114, Color.BLACK.getRGB());
-    }
-
-    @Override
-    public void setRecipe(@Nonnull IRecipeLayout layout, @Nonnull IInfuserRecipe recipe, @Nonnull IIngredients ii) {
-        layout.getItemStacks().init(0, true, 64, 52);
-        layout.getItemStacks().set(0, new ItemStack(ModBlocks.manaInfuser));
-        int index = 1;
-        double angleBetweenEach = 360.0D / (double)ii.getInputs(VanillaTypes.ITEM).size();
-        Vector2f point = new Vector2f(64.0F, 20.0F);
-        Vector2f center = new Vector2f(64.0F, 52.0F);
-
-        for(Iterator<List<ItemStack>> var9 = ii.getInputs(VanillaTypes.ITEM).iterator(); var9.hasNext(); point = PetalApothecaryRecipeCategory.rotatePointAbout(point, center, angleBetweenEach)) {
-            List<ItemStack> o = var9.next();
-            layout.getItemStacks().init(index, true, (int)point.x, (int)point.y);
-            layout.getItemStacks().set(index, o);
-            ++index;
-        }
-
-        layout.getItemStacks().init(index, false, 103, 17);
-        layout.getItemStacks().set(index, ii.getOutputs(VanillaTypes.ITEM).get(0));
     }
 }
