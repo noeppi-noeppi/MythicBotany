31602e5a44aae318fcb7bfe98cf311280b8d6db5
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-09-01 14:26:37 +0200

    Lexicon fixes

--- src/main/java/mythicbotany/data/patchouli/content/RitualContent.java
+++ src/main/java/mythicbotany/data/patchouli/content/RitualContent.java
@@ -4,10 +4,8 @@ import com.google.gson.JsonObject;
 import mythicbotany.data.patchouli.page.PageBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
-import java.util.Objects;
 
 public class RitualContent extends CaptionContent {
 
@@ -36,8 +34,7 @@ public class RitualContent extends CaptionContent {
         
         JsonObject json1 = new JsonObject();
         json1.addProperty("type", "mythicbotany:ritual_pattern");
-        ResourceLocation itemLoc = Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(this.item), "Item not registered: " + this.item);
-        json1.addProperty("title", "item." + itemLoc.getNamespace() + "." + itemLoc.getPath());
+        json1.addProperty("title", this.item.getDescriptionId());
         json1.addProperty("recipe", this.id.toString());
         builder.addPage(json1);
 
@@ -46,7 +43,7 @@ public class RitualContent extends CaptionContent {
         json2.addProperty("title", "");
         json2.addProperty("recipe", this.id.toString());
         if (caption != null) {
-            json2.addProperty("text", caption);
+            json2.addProperty("text", builder.translate(caption));
         }
         builder.addPage(json2);
     }
