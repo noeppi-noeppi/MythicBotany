8c11937b15c2202996016b3d9040420845364c72
noeppi_noeppi <noeppinoeppi@gmail.com>
2023-05-21 11:34:04 +0200

    Lexicon fixes

--- src/main/java/mythicbotany/alftools/GreatestAuraRing.java
+++ src/main/java/mythicbotany/alftools/GreatestAuraRing.java
@@ -14,7 +14,7 @@ public class GreatestAuraRing extends BaubleItem {
     }
 
     public void onWornTick(ItemStack stack, LivingEntity living) {
-        if (!living.level.isClientSide && living instanceof Player player) {
+        if (!living.level.isClientSide && living instanceof Player player && player.tickCount % 2 == 0) {
             ManaItemHandler.instance().dispatchManaExact(stack, player, 5, true);
         }
     }
